fa313d5e85ec1aaf7aef63cc95b692cf526b8507
00248 <102600924+00248@users.noreply.github.com>
2022-04-19 09:29:20 -0400

    4/19/2022
    The changes made today in the LEGO code was that the initial turn was added so that it will align with the vertical electrophoresis pole, after grabbing the first two rings. The biggest problem that needs to be fixed, is the motors don't drive at the same speed, so the robot tries to drive off of the board.

---
+++ main (1).c
@@ -0,0 +1,64 @@
+#include <kipr/wombat.h>
+#include <math.h>
+
+void set_servos(int s0_pos)
+{
+    //int s1_pos = round(((27*s0_pos)/28)-54);
+    int s1_pos = (s0_pos - 500);
+    set_servo_position(0, s0_pos);
+    set_servo_position(1, s1_pos);
+}
+
+int main()
+{
+    int claw_open = 1500;
+    int claw_closed = 950;
+    int claw_servo = 2;
+    int base_servo = 0;
+    int wrist_servo = 1;
+    int r1pos = 1685;
+    int r2pos = 1700;
+    int r3pos = 1925;
+    int carrying_pos = 1000;
+    
+    set_servos(r2pos);
+    enable_servos();
+    set_servo_position(claw_servo, claw_open);
+    msleep(2000);
+    while (analog(4)<1600)
+    {
+        mav(0, 250);
+   		mav(1, 250);
+        printf("sensor reads %d\n",analog(4));
+        msleep(100);
+    }
+    ao();
+    set_servo_position(claw_servo, claw_closed);
+    msleep(2000);
+    set_servos(carrying_pos);
+    msleep(2000);
+    mav(0, -700);
+    mav(1, -700);
+    msleep(10000);
+    ao();
+    while (analog(3)<2800)
+    {
+    mav(0, -700);
+    mav(1, -700);
+    msleep(200);
+    }
+    ao();
+    mav(0, 300);
+    mav(1,-500);
+    msleep(2700);
+    ao();
+     while (analog(3)<3200)
+    {
+    mav(0, -500);
+    mav(1, -500);
+    msleep(200);
+    }
+    ao();
+    disable_servos();
+    return 0;
+}
